0f6cd7eccdd0419fd14fe2f9798b5e2057cc8d29
jinwoo <fanystory@gmail.com>
2022-05-10 18:26:32 +0900

    업데이트

--- app.py
+++ app.py
@@ -26,6 +26,28 @@ SECRET_KEY = 'SPARTA'
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
+        """ 초기 무드 셋팅용임, 주석 풀면 안되요~ 나중에 자리 옮길 예정
+        docs = [[1, '🚶 길거리에서 흘러나오는 요즘 노래'],
+                [2, '☀️ 아침을 깨우는 굿모닝 뮤직'],
+                [3, '☕️ 나른한 오후 카페에 온것만 같은 편안한 무드'],
+                [4, '🧑🏻‍💻 일하면서 듣기 좋은 브금'],
+                [5, '🤩 내적댄스 유발하는 하우스 뮤직'],
+                [6, '💃 소리벗고 빤스질러'],
+                [7, '🌙 자기전에 꿀잠 예약하는 음악'],
+                [8, '☁️ 내 마음에 구름'],
+                [9, '🫠 아무생각 없을때 들어요'],
+                [10, '🤡 뭔진 모르겠는데 아무튼 신나'],
+                [11, '👀 선생님 혹은 부장님 몰래 듣는 조용한 음악'],
+                [12, '💆‍♂️ 마사지샵에서 나올것 같은 힐링힐링 무드'],
+                [13, '💪 득근용 브금']]
+
+        for doc in docs:
+            print(doc[0], doc[1])
+            mydata = {'mood_num':doc[0], 'mood_name':doc[1]}
+            db.mood_db.insert_one(mydata)
+            print('저장성공')
+        """
+
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.users.find_one({"username": payload['id']})
         #print(user_info)
--- static/js/common.js
+++ static/js/common.js
@@ -3,3 +3,64 @@ function logout() {
     alert('로그아웃!')
     window.location.href = '/login'
 }
+
+function onTop(){
+    $("html, body").animate({scrollTop: 0}, "slow");
+    return false;
+}
+
+function uploadPost(){
+    let doc_user_id = $('#post_userId').val();
+    let doc_post_title = $('#post_title').val();
+    let doc_post_data = $('#post_soundCloudData').val();
+    let doc_post_message = $('#post_message').val();
+    let doc_post_mood = $('#post_selectMood').val();
+    let doc_post_date = new Date();
+
+    console.log(doc_user_id, doc_post_title, doc_post_data, doc_post_message, doc_post_mood, doc_post_date);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+// Functions to open and close a modal
+    function openModal($el) {
+        $el.classList.add('is-active');
+    }
+
+    function closeModal($el) {
+        $el.classList.remove('is-active');
+    }
+
+    function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+            closeModal($modal);
+        });
+    }
+
+// Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', () => {
+            openModal($target);
+        });
+    });
+
+// Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+
+// Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+        const e = event || window.event;
+
+        if (e.keyCode === 27) { // Escape key
+            closeAllModals();
+        }
+    });
+});
